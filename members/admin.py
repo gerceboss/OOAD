ef3907753734814f2acb1e47a1c83eb04aee40b1
arorasiya008 <122709276+arorasiya008@users.noreply.github.com>
2023-11-04 23:54:13 +0530

    Registered models with admin site

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Users, ItemsOnBid, ItemsClaimed
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(ItemsOnBid)
+admin.site.register(ItemsClaimed)
